7a4add63694e110f96b24a2ced4bff7479359eb9
Stephen Walther <stephen.walther@superexpert.com>
2025-05-13 17:59:42 -0500

    Added child to the LoggerContract

--- src/logger-contract.ts
+++ src/logger-contract.ts
@@ -2,4 +2,5 @@ export interface LoggerContract {
   info (msg: string, meta?: Record<string, unknown>): Promise<void> | void;
   warn (msg: string, meta?: Record<string, unknown>): Promise<void> | void;
   error(err: Error, msg?: string): Promise<void> | void;
+  child(extra: Record<string, unknown>): LoggerContract;
 }
